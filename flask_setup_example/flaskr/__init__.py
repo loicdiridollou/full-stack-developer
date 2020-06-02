0a2050b9bdd4b09f9ecd30410bd88700749c4a4b
Loic Diridollou <loic.diridollou@gmail.com>
2020-06-02 13:55:13 -0700

    Upgrading the features

--- flask_setup_example/flaskr/__init__.py
+++ flask_setup_example/flaskr/__init__.py
@@ -1,24 +1,39 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 import os
+from flask_cors import CORS
+from models import setup_db, Plant
 
 def create_app(test_config = None):
     app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
 
-    app.config.from_mapping(SECRET_KEY='dev', DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'))
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
     
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass   
-
     @app.route('/')
-    def hello_world():
-        return jsonify({'message':'Hello, World!'})
-    
+    def hello():
+        return jsonify({'message': 'Hello World!'})
+
+    @app.route('/plants')
+    def get_plants():
+        plants = Plant.query.all()
+        formatted_plants = [plant.format() for plant in plants]
+
+
+        return jsonify({'success': True,
+                        'plants': formatted_plants})
+
+    @app.route('/plants/<int:plant_id>')
+    def get_specific_plant(plant_id):
+        plant = Plant.query.filter(Plant.id == plant_id).one_or_none()
+        if plant is None:
+            abort(404)
+        else:
+            return jsonify({'success': True, 'plant': plant.format()})
+
     return app
