13843700ea01b0b83e9b528bb61d1f85c8bd9c61
Loic Diridollou <loic.diridollou@gmail.com>
2020-06-02 18:53:06 -0700

    Changing the database

--- flask_setup_example/models.py
+++ flask_setup_example/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "bookshelf"
+database_name = "bookshelf_test"
 database_path ="postgres://{}:{}@{}/{}".format('student', 'student','localhost:5432', database_name)
 
 db = SQLAlchemy()
