8bab28bf1d9f0bba9d7e3d3a4b62dc92764b3410
Loic Diridollou <loic.diridollou@gmail.com>
2020-06-05 18:08:44 -0700

    Quick fix

--- 02_trivia_api/frontend/src/components/QuizView.js
+++ 02_trivia_api/frontend/src/components/QuizView.js
@@ -111,7 +111,7 @@ class QuizView extends Component {
                       key={id}
                       value={id}
                       className="play-category"
-                      onClick={() => this.selectCategory({type:this.state.categories[id], id})}>
+                      onClick={() => this.selectCategory({type:this.state.categories[id], id:this.state.categories[id]})}>
                       {this.state.categories[id]}
                     </div>
                   )
