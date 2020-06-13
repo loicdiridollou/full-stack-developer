1a4c34f8d6ad4899f92b245cc23ffea60500d804
Loic Diridollou <loic.diridollou@gmail.com>
2020-06-13 16:20:59 -0700

    Listing redirection parameters

--- 03_coffee_shop_full_stack/frontend/src/environments/environment.ts
+++ 03_coffee_shop_full_stack/frontend/src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'udacity-projects.auth0.com', // the auth0 domain prefix
-    audience: 'dev', // the audience set for the auth0 app
+    url: 'udacity-projects', // the auth0 domain prefix
+    audience: 'image', // the audience set for the auth0 app
     clientId: 'IU8kVrBkQbwmtBMNIygG5ZrnH4CrTnTp', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
