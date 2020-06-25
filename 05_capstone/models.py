ae7c64a7705480b9320017cd3efc036f671df31a
Loic Diridollou <loic.diridollou@gmail.com>
2020-06-25 11:26:31 -0700

    Minor corrections

--- 05_capstone/models.py
+++ 05_capstone/models.py
@@ -10,25 +10,19 @@ database_path = os.environ.get('DATABASE_URL', "postgres://{}:{}@{}/{}".format(d
 
 db = SQLAlchemy()
 
-def setup_db(app, database_path=database_path):
-    '''binds a flask application and a SQLAlchemy service'''
+def db_setup(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
     db.create_all()
 
-def db_drop_and_create_all():
-    '''drops the database tables and starts fresh
-    can be used to initialize a clean database
-    '''
+def db_reset():
     db.drop_all()
     db.create_all()
-    db_init_records()
+    db_populate()
 
-def db_init_records():
-    '''this will initialize the database with some test records.'''
-
+def db_populate():
     new_actor = (Actor(
         name = 'Anne',
         gender = 'Female',
@@ -40,30 +34,15 @@ def db_init_records():
         release_date = date.today()
         ))
 
-    # new_performance = Performance.insert().values(
-    #     Movie_id = new_movie.id,
-    #     Actor_id = new_actor.id,
-    #     actor_fee = 500.00
-    # )
-
     new_actor.insert()
     new_movie.insert()
-    #db.session.execute(new_performance) 
     db.session.commit()
 
-#----------------------------------------------------------------------------#
-# Performance Junction Object N:N 
-#----------------------------------------------------------------------------#
 
-# Performance = db.Table('Performance', db.Model.metadata,
-#     db.Column('Movie_id', db.Integer, db.ForeignKey('movies.id')),
-#     db.Column('Actor_id', db.Integer, db.ForeignKey('actors.id')),
-#     db.Column('actor_fee', db.Float)
-# )
-
-#----------------------------------------------------------------------------#
-# Actors Model 
-#----------------------------------------------------------------------------#
+########################################
+# Actors model
+# id, name, gender, age
+########################################
 
 class Actor(db.Model):  
   __tablename__ = 'actors'
@@ -97,9 +76,11 @@ class Actor(db.Model):
       'age': self.age
     }
 
-#----------------------------------------------------------------------------#
-# Movies Model 
-#----------------------------------------------------------------------------#
+
+########################################
+# Movies model
+# id, title, release_date, actors
+########################################
 
 class Movie(db.Model):  
   __tablename__ = 'movies'
@@ -107,7 +88,7 @@ class Movie(db.Model):
   id = Column(Integer, primary_key=True)
   title = Column(String)
   release_date = Column(Date)
-  actors = Column(String) #db.relationship('Actor', secondary=Performance, backref=db.backref('performances', lazy='joined'))
+  actors = Column(String)
 
   def __init__(self, title, release_date) :
     self.title = title
@@ -130,5 +111,3 @@ class Movie(db.Model):
       'title' : self.title,
       'release_date': self.release_date
     }
-  
-  # Instead of creating a new Table, the documentation recommends to create a association table
