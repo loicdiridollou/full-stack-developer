69d18039665b1f22036ed807311ac05fc85a88a8
Loic Diridollou <loic.diridollou@gmail.com>
2024-07-19 18:25:27 -0400

    Updating security features

--- flask_hello_app.py
+++ flask_hello_app.py
@@ -1,27 +1,35 @@
+import os
+from typing import cast
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://joe:test@localhost:5432/example'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+password = os.environ.get("PASSWORD")
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"postgresql://joe:{password}@localhost:5432/example"
+)
 db = SQLAlchemy(app)
 
 
 class Person(db.Model):
-    __tablename__ = 'persons'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(), nullable = False)
+    __tablename__ = "persons"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
 
     def __repr__(self):
-        return f'<Person ID: {self.id}, name: {self.name}>'
+        return f"<Person ID: {self.id}, name: {self.name}>"
+
 
 db.create_all()
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    person = Person.query.first()
-    return 'Hello ' + person.name
+    person = cast(Person, Person.query.first())
+    return "Hello " + person.name
+
 
 if __name__ == "__main__":
     app.run()
--- todoapp/app.py
+++ todoapp/app.py
@@ -1,46 +1,49 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-
+import os
 import sys
 
+from flask import (Flask, abort, jsonify, redirect, render_template, request,
+                   url_for)
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://udacity:udacity@localhost:5432/todoapp'
+password = os.environ.get("PASSWORD")
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"postgresql://udacity:{password}@localhost:5432/todoapp"
+)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
 class Todo(db.Model):
-    __tablename__ = 'todos'
-    id = db.Column(db.Integer, primary_key = True)
-    description = db.Column(db.String, nullable = False)
-    completed = db.Column(db.Boolean, nullable = False, default = False)
-    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable = False)
-
+    __tablename__ = "todos"
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String, nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey("todolists.id"), nullable=False)
 
     def __repr__(self):
-        return f'<Todo {self.id} {self.description}>'
+        return f"<Todo {self.id} {self.description}>"
 
 
 class TodoList(db.Model):
-    __tablename__ = 'todolists'
+    __tablename__ = "todolists"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable = False)
-    todos = db.relationship('Todo', backref = 'list', lazy = True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship("Todo", backref="list", lazy=True)
 
 
-@app.route('/todos/create', methods = ['POST'])
+@app.route("/todos/create", methods=["POST"])
 def create_todo():
     error = False
     body = {}
     try:
         description = request.get_json()["description"]
         list_id = request.get_json()["list_id"]
-        todo = Todo(description = description, list_id = list_id)
+        todo = Todo(description=description, list_id=list_id)
         db.session.add(todo)
         db.session.commit()
-        body['description'] = todo.description
+        body["description"] = todo.description
     except:
         error = True
         db.session.rollback()
@@ -50,17 +53,18 @@ def create_todo():
     if not error:
         return jsonify(body)
 
-@app.route('/lists/create', methods = ['POST'])
+
+@app.route("/lists/create", methods=["POST"])
 def create_list():
     error = False
     body = {}
     try:
         name = request.get_json()["list_name"]
-        todolist = TodoList(name = name)
+        todolist = TodoList(name=name)
         db.session.add(todolist)
         db.session.commit()
-        body['name'] = todolist.name
-        body['id'] = todolist.id
+        body["name"] = todolist.name
+        body["id"] = todolist.id
         print(todolist.name)
     except:
         error = True
@@ -72,10 +76,10 @@ def create_list():
         return jsonify(body)
 
 
-@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+@app.route("/todos/<todo_id>/set-completed", methods=["POST"])
 def set_todos_completed(todo_id):
     try:
-        completed = request.get_json(force = True)['completed']
+        completed = request.get_json(force=True)["completed"]
         todo = Todo.query.get(todo_id)
         todo.completed = completed
         db.session.commit()
@@ -83,9 +87,10 @@ def set_todos_completed(todo_id):
         db.session.rollback()
     finally:
         db.session.close()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-@app.route('/todos/<todo_id>/delete', methods=['DELETE'])
+
+@app.route("/todos/<todo_id>/delete", methods=["DELETE"])
 def delete_todo(todo_id):
     try:
         Todo.query.filter_by(id=todo_id).delete()
@@ -94,20 +99,20 @@ def delete_todo(todo_id):
         db.session.rollback()
     finally:
         db.session.close()
-    return jsonify({ 'success': True })
+    return jsonify({"success": True})
 
 
-@app.route('/lists/<list_id>/delete', methods=['DELETE'])
+@app.route("/lists/<list_id>/delete", methods=["DELETE"])
 def delete_list(list_id):
     error = False
     try:
         lists = TodoList.query.get(list_id)
         for todo in lists.todos:
             db.session.delete(todo)
-        
+
         db.session.delete(lists)
         db.session.commit()
-    except():
+    except ():
         db.session.rollback()
         error = True
     finally:
@@ -115,19 +120,22 @@ def delete_list(list_id):
     if error:
         abort(500)
     else:
-        return jsonify({'success': True})
+        return jsonify({"success": True})
 
 
-
-@app.route('/lists/<list_id>')
+@app.route("/lists/<list_id>")
 def get_list_todo(list_id):
-    return render_template('index.html', todos = Todo.query.filter_by(list_id = list_id).order_by('id').all(), lists = TodoList.query.all(),
-            active_list = TodoList.query.get(list_id))
+    return render_template(
+        "index.html",
+        todos=Todo.query.filter_by(list_id=list_id).order_by("id").all(),
+        lists=TodoList.query.all(),
+        active_list=TodoList.query.get(list_id),
+    )
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return redirect(url_for('get_list_todo', list_id = 1))
+    return redirect(url_for("get_list_todo", list_id=1))
 
 
 if __name__ == "__main__":
